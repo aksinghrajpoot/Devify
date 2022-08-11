a97a2576da71df0c2c2e2ac47b496b97fcea3623
aksinghrajpoot <imaksinghrajpoot@gmail.com.com>
2022-08-11 19:24:52 +0530

    v-0.0.1

--- pages/hosting.js
+++ pages/hosting.js
@@ -15,7 +15,10 @@ export default function Hosting() {
             {hostingData
               .sort((a, b) => a.title.localeCompare(b.title))
               .map((item) => (
-                <div className="p-6 border border-yellow-300/40 rounded-lg shadow-md shadow-yellow-300/20 relative">
+                <div
+                  key={item.title}
+                  className="p-6 border border-yellow-300/40 rounded-lg shadow-md shadow-yellow-300/20 relative"
+                >
                   <h3 className="font-bold text-lg text-teal-300 mb-2">
                     {item.title}
                   </h3>
@@ -39,11 +42,11 @@ export default function Hosting() {
                       viewBox="0 0 16 16"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
                       />
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
                       />
                     </svg>
